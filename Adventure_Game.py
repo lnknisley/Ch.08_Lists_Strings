c5eca1474807f32b50d6c188444369d9991b9183
lnknisley <andthepandabomb@gmail.com>
2024-12-12 14:15:21 -0600

    Did it

--- Adventure_Game.py
+++ Adventure_Game.py
@@ -4,5 +4,11 @@ ADVENTURE GAME (4pts)
 1.) Use the pseudo-code on the website to help you set up the basic move through the house program
 2.) Print off a physical map for players to use with your program
 3.) Expand your program to make it a real adventure game
-
 '''
+
+room_list = []
+room = ["", ]
+
+"""
+
+"""
